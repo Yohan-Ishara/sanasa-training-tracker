2b54c27470eef54f575da09ba417827092838041
Yohan Ishara <yohanishara01@gmail.com>
2025-11-07 22:17:43 +0530

    initial commit

---
+++ src/main/java/com/sanasa/sanasatrainingtracker/SanasaTrainingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sanasa.sanasatrainingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanasaTrainingTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanasaTrainingTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanasa/sanasatrainingtracker/SanasaTrainingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanasa.sanasatrainingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanasaTrainingTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
